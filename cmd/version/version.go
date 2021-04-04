0330927faf926e3bd6909126e41e790cab4cb991
linxuyalun <froggy2818@gmail.com>
2021-04-04 14:41:30 +0800

    feat: Add version

---
+++ cmd/version/version.go
@@ -0,0 +1,51 @@
+package version
+
+import (
+	"log"
+	"runtime"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	AppVersion            = ""
+	LocalSchedulerVersion = ""
+	BuildTime             = ""
+	GitCommit             = ""
+)
+
+type VersionInfo struct {
+	AppVersion            string
+	LocalSchedulerVersion string
+	GoVersion             string
+	Compiler              string
+	Platform              string
+	BuildTime             string
+	GitCommit             string
+}
+
+func (info *VersionInfo) String() string {
+	return "{Tass Version: " + info.AppVersion + ", Local scheduler version: " + info.LocalSchedulerVersion +
+		", Go version: " + info.GoVersion + ", Compiler version: " + info.Compiler + ", Platform: " + info.Platform +
+		", Build time: " + info.BuildTime + ", Git commit: " + info.GitCommit + "}"
+}
+
+var VersionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Version of the Tass.",
+	Long:  "Version information of the Tass.",
+	Run: func(cmd *cobra.Command, args []string) {
+		info := &VersionInfo{
+			AppVersion:            AppVersion,
+			LocalSchedulerVersion: LocalSchedulerVersion,
+			GoVersion:             runtime.Version(),
+			Compiler:              runtime.Compiler,
+			Platform:              runtime.GOOS + "/" + runtime.GOARCH,
+			BuildTime:             BuildTime,
+			GitCommit:             GitCommit,
+		}
+		log.Println(info.String())
+	},
+}
+
+func init() {}
