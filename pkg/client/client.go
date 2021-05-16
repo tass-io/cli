5e0ce5a71e81f1bb304652e1aa3fe43db84754a5
linxuyalun <froggy2818@gmail.com>
2021-05-16 03:26:56 +0000

    feat: networking k8s client initiation

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,23 +1,30 @@
 package client
 
 import (
-	"github.com/tass-io/cli/pkg/logging"
 	"os"
 
+	"github.com/tass-io/cli/pkg/logging"
 	"k8s.io/apimachinery/pkg/runtime"
 
 	serverlessv1alpha1 "github.com/tass-io/tass-operator/api/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
+	networkingv1beta1 "k8s.io/api/networking/v1beta1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 )
 
-var c *client.Client
+var tassClient *client.Client
+var netClient *client.Client
 var log = logging.Log
 
 // GetCRDClient returns the CRD client pointer
 func GetCRDClient() *client.Client {
-	return c
+	return tassClient
+}
+
+// GetNetClient returns the Net client pointer
+func GetNetClinet() *client.Client {
+	return netClient
 }
 
 // initCRDClient create a client to perform CRUD operations as well as default Kind on a Kubernetes cluster.
@@ -38,9 +45,27 @@ func initCRDClient() {
 		log.Error("failed to create client")
 		os.Exit(1)
 	}
-	c = &cl
+	tassClient = &cl
+}
+
+// initNetClient create a client to perform CRUD operations as well as default Kind on a Kubernetes cluster.
+// In order to call the recognize Net types,
+// a scheme that corresponding to `networking` types registered for the Client is set.
+func initNetClient() {
+	scheme := runtime.NewScheme()
+	if err := networkingv1beta1.AddToScheme(scheme); err != nil {
+		log.Error(err)
+		os.Exit(1)
+	}
+	cl, err := client.New(config.GetConfigOrDie(), client.Options{Scheme: scheme})
+	if err != nil {
+		log.Error(err)
+		os.Exit(1)
+	}
+	netClient = &cl
 }
 
 func init() {
 	initCRDClient()
+	initNetClient()
 }
